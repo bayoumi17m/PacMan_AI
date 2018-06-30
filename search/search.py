8834466bdba90bc74d8bff7da2f5aa7612e9d57d
Magd <mb2363@cornell.edu>
2018-06-29 22:34:08 -0400

    BFS Termination Error

--- search/search.py
+++ search/search.py
@@ -115,9 +115,9 @@ def depthFirstSearch(problem):
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
-    theFringe = util.Queue();
-    expanded = set();
-    theFringe.push((problem.getStartState(),[]));
+    theFringe = util.Queue()
+    expanded = set()
+    theFringe.push((problem.getStartState(),[]))
 
     while not (theFringe.isEmpty()):
 
@@ -134,7 +134,7 @@ def breadthFirstSearch(problem):
         for state, direction, cost in problem.getSuccessors(popState):
             if (state in expanded):
                 continue
-            theFringe.push(popState, popMoves+[direction])
+            theFringe.push((popState, popMoves+[direction]))
 
     return []
 
